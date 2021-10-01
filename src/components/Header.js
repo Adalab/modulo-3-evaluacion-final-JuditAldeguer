8fd73caf8259b955fb7915d47515efab9e2889d9
JuditAldeguer <jud.vicens@hotmail.es>
2021-10-01 16:34:35 +0200

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,11 @@
 import '../styles/layout/header.scss';
 //image
 import logo from '../images/Rick_and_Morty_-_logo.png';
-//Components
-import Links from './secondary-components/Links';
 
 const Header = (props) => {
   return (
     <header>
       <img src={logo} alt="Rick and Morty" />
-      <Links productId={props.productId} />
     </header>
   );
 };
