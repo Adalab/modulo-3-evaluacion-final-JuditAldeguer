34203aa4c1a54e3c8a8c6845e1f2570159d83cf4
JuditAldeguer <jud.vicens@hotmail.es>
2021-10-01 17:39:32 +0200

    add Loading component

--- src/components/App.js
+++ src/components/App.js
@@ -12,21 +12,28 @@ import '../styles/App.scss';
 //Components
 import Header from './Header';
 import Footer from './Footer';
-import NotFoundPage from './secondary-components/NotFoundPage';
 import CharacterDetail from './CharacterDetail';
 import Filters from './Filters';
 import CharacterList from './CharacterList';
+import NotFoundPage from './secondary-components/NotFoundPage';
+import Loading from './secondary-components/Loading';
 
 function App() {
   //useState
+  const [listCharacters, setListCharacters] = useState([]);
+  const [searchWord, setSearchWord] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   //useEffect
-  useEffect(
+  useEffect(() => {
+    setIsLoading(true);
     callToApi().then((response) => {
-      console.log(response);
-    }),
-    []
-  );
+      setListCharacters(response);
+      setIsLoading(false);
+    });
+  }, [searchWord]);
+  console.log(listCharacters);
+
   //useRef
   const routeData = useRouteMatch('/character-detail/:characterId');
   const characterId = routeData !== null ? routeData.params.productId : '';
@@ -40,6 +47,7 @@ function App() {
     <div className="page">
       <Header productId="321" />
       <main>
+        <Loading loading={isLoading} />
         <h1>Bienvenid@, encuentra tu(s) personaje(s) favorito(s)</h1>
         <Filters></Filters>
         <CharacterList />
